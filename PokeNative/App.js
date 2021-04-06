3648e716c5a5727f899f6e0173ab530f4d514355
Patrick <patrick_p12@hotmail.com>
2021-04-06 11:58:48 +0200

    Update beginscherm

--- PokeNative/App.js
+++ PokeNative/App.js
@@ -1,11 +1,26 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { StyleSheet, Text, View, Image, SafeAreaView } from "react-native";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image
+        source={{
+          width: 291,
+          height: 107,
+          uri:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/1280px-International_Pok%C3%A9mon_logo.svg.png",
+        }}
+      ></Image>
+      <Text>CATCH!</Text>
+      <Image
+        source={{
+          width: 255,
+          height: 255,
+          uri: "https://pngimg.com/uploads/pokeball/pokeball_PNG21.png",
+        }}
+      ></Image>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
@@ -17,5 +32,6 @@ const styles = StyleSheet.create({
     backgroundColor: "#A5E0FF",
     alignItems: "center",
     justifyContent: "center",
+    padding: 20,
   },
 });
