ac7d5e46077af4019b02853ecea7ba92e0f2c47f
Patrick <patrick_p12@hotmail.com>
2021-04-08 14:11:46 +0200

    Update screens

--- PokeNative/App.js
+++ PokeNative/App.js
@@ -1,54 +1,7 @@
 import React from "react";
-import {
-  Dimensions,
-  StyleSheet,
-  Text,
-  View,
-  Image,
-  SafeAreaView,
-  Platform,
-  StatusBar,
-} from "react-native";
+import { View } from "react-native";
+import SplashScreen from "./app/screens/SplashScreen";
 
 export default function App() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <Image
-        source={{
-          width: 291,
-          height: 107,
-          uri:
-            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/1280px-International_Pok%C3%A9mon_logo.svg.png",
-        }}
-      ></Image>
-      <Text style={styles.textsplash}>CATCH!</Text>
-      <Image
-        style={styles.splashimage}
-        source={{
-          width: 255,
-          height: 255,
-          uri: "https://pngimg.com/uploads/pokeball/pokeball_PNG21.png",
-        }}
-      ></Image>
-      <StatusBar style="auto" />
-    </SafeAreaView>
-  );
+  return <SplashScreen />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#A5E0FF",
-    alignItems: "center",
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-  },
-  textsplash: {
-    fontSize: 52,
-    color: "#fff",
-    fontStyle: "italic",
-    paddingTop: 20,
-  },
-  splashimage: {
-    marginTop: 80,
-  },
-});
---
+++ PokeNative/app/screens/TutorialScherm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { ImageBackground } from 'react-native';
+
+function TutorialScherm(props) {
+    return (
+            
+
+    );
+}
+
+export default TutorialScherm;
