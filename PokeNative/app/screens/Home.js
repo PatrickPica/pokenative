8ee81b53221b0f24bf88825e366fc9fc7fa0f324
Patrick <patrick_p12@hotmail.com>
2021-04-16 12:08:40 +0200

    API WERKEND! En styling

--- PokeNative/app/screens/Home.js
+++ PokeNative/app/screens/Home.js
@@ -1,7 +1,14 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import React, {useState} from "react";
+import React, { useState } from "react";
+import { LinearGradient } from "expo-linear-gradient";
 import { Button, ImageBackground } from "react-native";
+import {
+  useFonts,
+  Rubik_400Regular,
+  Rubik_400Regular_Italic,
+} from "@expo-google-fonts/rubik";
+import { AppLoading } from "expo";
 import {
   Dimensions,
   StyleSheet,
@@ -15,25 +22,12 @@ import {
 } from "react-native";
 
 export default function Home() {
-  return (
-    <View>
-      <Text></Text>
-      <Button title="Zoek Pokemon" onPress={this.randomPokemon}></Button>
-    </View>
-  )
-
-
-const styles = StyleSheet.create({})
-
-
-function lekkerpokemon() {
   const [pokemon, setPokemon] = useState("Start your adventure!");
   const [pokemonImg, setPokemonImg] = useState();
   const [wildpokemon, setWildpokemon] = useState("");
   const [typepokemon, setTypepokemon] = useState();
   const [nummerpokemon, setNummerpokemon] = useState();
 
-  
   function randomPokemon() {
     const randNum = Math.abs(Math.floor(Math.random() * (0 - 151)));
     fetch(`https://pokeapi.co/api/v2/pokemon/${randNum}`)
@@ -49,5 +43,35 @@ function lekkerpokemon() {
       .catch(console.error);
   }
 
+  let [fontsLoaded, error] = useFonts({
+    Rubik_400Regular,
+  });
+  return (
+    <LinearGradient colors={["#08B6B6", "#045B5B"]} style={styles.background}>
+      <SafeAreaView style={styles.encounter}>
+        <Text style={styles.appeared}>{wildpokemon}</Text>
+        <Text style={styles.nummer}>#{nummerpokemon}</Text>
+        <Text style={styles.pokemonnaam}>{pokemon}</Text>
+        <Text></Text>
+        <Button title="Zoek Pokemon" onPress={randomPokemon}></Button>
+      </SafeAreaView>
+    </LinearGradient>
+  );
 }
-}
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  encounter: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  pokemonnaam: {
+    fontFamily: "Rubik_400Regular",
+    fontSize: 30,
+    color: "#fff",
+  },
+});
