036ac9967550e3adcd948b3e07135c0896b4dd80
Patrick <patrick_p12@hotmail.com>
2021-04-20 13:48:42 +0200

    Update Pokedex

--- PokeNative/app/screens/Pokedex.js
+++ PokeNative/app/screens/Pokedex.js
@@ -1,12 +1,13 @@
 import React from 'react'
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView, StyleSheet, Text, View, TouchableOpacity, Image, Button } from 'react-native'
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
-export default function pokedex() {
+export default function pokedex({navigation}) {
     return (
         <SafeAreaView style={styles.background}>
             <Text>hoi</Text>
+            <Button title="GO BACK OUT THERE!" style={styles.terugknop} onPress={() => navigation.goBack("Home")}></Button>
         </SafeAreaView>
     )
 }
@@ -14,5 +15,16 @@ export default function pokedex() {
 const styles = StyleSheet.create({
     background: {
         flex: 1,
+        backgroundColor: "#DC0916",
+        justifyContent: "center",
+        alignItems: "center",
     },
+    terugknop: {
+        width: 215,
+        height: 53,
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 27,
+        margin: 20,
+      },
 })
